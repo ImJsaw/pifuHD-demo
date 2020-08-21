129f13bbd63655775b5f8e0dc703e3cffaca5edb
賓 <bing5300@gmail.com>
2020-08-21 15:13:45 +0800

    add generate UV feature, integrate pifu,clean mesh,generate UV to simple_pifu

--- apps/recon.py
+++ apps/recon.py
@@ -1,34 +1,34 @@
 # Copyright (c) Facebook, Inc. and its affiliates. All rights reserved.
 
+import json
+from PIL import Image
+from lib.geometry import index
+from lib.model import HGPIFuNetwNML, HGPIFuMRNet
+from lib.data import EvalWPoseDataset, EvalDataset
+from lib.mesh_util import save_obj_mesh_with_color, reconstruction
+from lib.options import BaseOptions
+from numpy.linalg import inv
+import matplotlib
+import matplotlib.cm as cm
+import matplotlib.pyplot as plt
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+import torch.nn as nn
+import torch
+import random
+import cv2
+import numpy as np
+import time
 import sys
 import os
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.insert(0, os.path.abspath(
+    os.path.join(os.path.dirname(__file__), '..')))
 ROOT_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-import time
-import json 
-import numpy as np
-import cv2
-import random
-import torch
-import torch.nn as nn
-from tqdm import tqdm
-from torch.utils.data import DataLoader
-import matplotlib.pyplot as plt
-import matplotlib.cm as cm
-import matplotlib
-from numpy.linalg import inv
-
-from lib.options import BaseOptions
-from lib.mesh_util import save_obj_mesh_with_color, reconstruction
-from lib.data import EvalWPoseDataset, EvalDataset
-from lib.model import HGPIFuNetwNML, HGPIFuMRNet
-from lib.geometry import index
-
-from PIL import Image
 
 parser = BaseOptions()
+
 
 def gen_mesh(res, net, cuda, data, save_path, thresh=0.5, use_octree=True, components=False):
     image_tensor_global = data['img_512'].to(device=cuda)
@@ -36,34 +36,39 @@ def gen_mesh(res, net, cuda, data, save_path, thresh=0.5, use_octree=True, compo
     calib_tensor = data['calib'].to(device=cuda)
 
     net.filter_global(image_tensor_global)
-    net.filter_local(image_tensor[:,None])
+    net.filter_local(image_tensor[:, None])
 
     try:
         if net.netG.netF is not None:
-            image_tensor_global = torch.cat([image_tensor_global, net.netG.nmlF], 0)
+            image_tensor_global = torch.cat(
+                [image_tensor_global, net.netG.nmlF], 0)
         if net.netG.netB is not None:
-            image_tensor_global = torch.cat([image_tensor_global, net.netG.nmlB], 0)
+            image_tensor_global = torch.cat(
+                [image_tensor_global, net.netG.nmlB], 0)
     except:
         pass
-    
+
     b_min = data['b_min']
     b_max = data['b_max']
     try:
         save_img_path = save_path[:-4] + '.png'
         save_img_list = []
         for v in range(image_tensor_global.shape[0]):
-            save_img = (np.transpose(image_tensor_global[v].detach().cpu().numpy(), (1, 2, 0)) * 0.5 + 0.5)[:, :, ::-1] * 255.0
+            save_img = (np.transpose(image_tensor_global[v].detach(
+            ).cpu().numpy(), (1, 2, 0)) * 0.5 + 0.5)[:, :, ::-1] * 255.0
             save_img_list.append(save_img)
         save_img = np.concatenate(save_img_list, axis=1)
         cv2.imwrite(save_img_path, save_img)
 
         verts, faces, _, _ = reconstruction(
             net, cuda, calib_tensor, res, b_min, b_max, thresh, use_octree=use_octree, num_samples=50000)
-        verts_tensor = torch.from_numpy(verts.T).unsqueeze(0).to(device=cuda).float()
+        verts_tensor = torch.from_numpy(
+            verts.T).unsqueeze(0).to(device=cuda).float()
         # if 'calib_world' in data:
         #     calib_world = data['calib_world'].numpy()[0]
         #     verts = np.matmul(np.concatenate([verts, np.ones_like(verts[:,:1])],1), inv(calib_world).T)[:,:3]
 
+        #print(calib_tensor)
         color = np.zeros(verts.shape)
         interval = 50000
         for i in range(len(color) // interval + 1):
@@ -72,9 +77,16 @@ def gen_mesh(res, net, cuda, data, save_path, thresh=0.5, use_octree=True, compo
                 right = -1
             else:
                 right = (i + 1) * interval
-            net.calc_normal(verts_tensor[:, None, :, left:right], calib_tensor[:,None], calib_tensor)
+            net.calc_normal(
+                verts_tensor[:, None, :, left:right], calib_tensor[:, None], calib_tensor)
             nml = net.nmls.detach().cpu().numpy()[0] * 0.5 + 0.5
             color[left:right] = nml.T
+        
+        if 'calib_world' in data:
+            calib_world = data['calib_world'].numpy()[0]
+            verts = np.matmul(np.concatenate(
+                [verts, np.ones_like(verts[:, :1])], 1), inv(calib_world).T)[:, :3]
+
 
         save_obj_mesh_with_color(save_path, verts, faces, color)
     except Exception as e:
@@ -87,13 +99,15 @@ def gen_mesh_imgColor(res, net, cuda, data, save_path, thresh=0.5, use_octree=Tr
     calib_tensor = data['calib'].to(device=cuda)
 
     net.filter_global(image_tensor_global)
-    net.filter_local(image_tensor[:,None])
+    net.filter_local(image_tensor[:, None])
 
     try:
         if net.netG.netF is not None:
-            image_tensor_global = torch.cat([image_tensor_global, net.netG.nmlF], 0)
+            image_tensor_global = torch.cat(
+                [image_tensor_global, net.netG.nmlF], 0)
         if net.netG.netB is not None:
-            image_tensor_global = torch.cat([image_tensor_global, net.netG.nmlB], 0)
+            image_tensor_global = torch.cat(
+                [image_tensor_global, net.netG.nmlB], 0)
     except:
         pass
 
@@ -103,24 +117,31 @@ def gen_mesh_imgColor(res, net, cuda, data, save_path, thresh=0.5, use_octree=Tr
         save_img_path = save_path[:-4] + '.png'
         save_img_list = []
         for v in range(image_tensor_global.shape[0]):
-            save_img = (np.transpose(image_tensor_global[v].detach().cpu().numpy(), (1, 2, 0)) * 0.5 + 0.5)[:, :, ::-1] * 255.0
+            save_img = (np.transpose(image_tensor_global[v].detach(
+            ).cpu().numpy(), (1, 2, 0)) * 0.5 + 0.5)[:, :, ::-1] * 255.0
             save_img_list.append(save_img)
         save_img = np.concatenate(save_img_list, axis=1)
         cv2.imwrite(save_img_path, save_img)
 
         verts, faces, _, _ = reconstruction(
             net, cuda, calib_tensor, res, b_min, b_max, thresh, use_octree=use_octree, num_samples=100000)
-        verts_tensor = torch.from_numpy(verts.T).unsqueeze(0).to(device=cuda).float()
+        verts_tensor = torch.from_numpy(
+            verts.T).unsqueeze(0).to(device=cuda).float()
 
         # if this returns error, projection must be defined somewhere else
         xyz_tensor = net.projection(verts_tensor, calib_tensor[:1])
         uv = xyz_tensor[:, :2, :]
+        print('uv', uv)
+        print('xyz_tensor', xyz_tensor)
+        print('verts', verts)
+
         color = index(image_tensor[:1], uv).detach().cpu().numpy()[0].T
         color = color * 0.5 + 0.5
 
         if 'calib_world' in data:
             calib_world = data['calib_world'].numpy()[0]
-            verts = np.matmul(np.concatenate([verts, np.ones_like(verts[:,:1])],1), inv(calib_world).T)[:,:3]
+            verts = np.matmul(np.concatenate(
+                [verts, np.ones_like(verts[:, :1])], 1), inv(calib_world).T)[:, :3]
 
         save_obj_mesh_with_color(save_path, verts, faces, color)
 
@@ -134,24 +155,26 @@ def recon(opt, use_rect=False):
     if opt.load_netMR_checkpoint_path is not None:
         state_dict_path = opt.load_netMR_checkpoint_path
     elif opt.resume_epoch < 0:
-        state_dict_path = '%s/%s_train_latest' % (opt.checkpoints_path, opt.name)
+        state_dict_path = '%s/%s_train_latest' % (
+            opt.checkpoints_path, opt.name)
         opt.resume_epoch = 0
     else:
-        state_dict_path = '%s/%s_train_epoch_%d' % (opt.checkpoints_path, opt.name, opt.resume_epoch)
-    
+        state_dict_path = '%s/%s_train_epoch_%d' % (
+            opt.checkpoints_path, opt.name, opt.resume_epoch)
+
     start_id = opt.start_id
     end_id = opt.end_id
 
     state_dict = None
     if state_dict_path is not None and os.path.exists(state_dict_path):
         print('Resuming from ', state_dict_path)
-        state_dict = torch.load(state_dict_path)    
+        state_dict = torch.load(state_dict_path)
         print('Warning: opt is overwritten.')
         dataroot = opt.dataroot
         resolution = opt.resolution
         results_path = opt.results_path
         loadSize = opt.loadSize
-        
+
         opt = state_dict['opt']
         opt.dataroot = dataroot
         opt.resolution = resolution
@@ -159,7 +182,7 @@ def recon(opt, use_rect=False):
         opt.loadSize = loadSize
     else:
         raise Exception('failed loading state dict!', state_dict_path)
-    
+
     # parser.print_options(opt)
 
     cuda = torch.device('cuda:%d' % opt.gpu_id)
@@ -184,41 +207,45 @@ def recon(opt, use_rect=False):
 
     os.makedirs(opt.checkpoints_path, exist_ok=True)
     os.makedirs(opt.results_path, exist_ok=True)
-    os.makedirs('%s/mResult' % (opt.results_path), exist_ok=True)
+    os.makedirs('%s/' % (opt.results_path), exist_ok=True)
 
     if start_id < 0:
         start_id = 0
     if end_id < 0:
         end_id = len(test_dataset)
 
-    ## test
+    # test
     with torch.no_grad():
         set_eval()
 
-        print('generate mesh ...')
+        print('generate mesh  ...')
         for i in tqdm(range(start_id, end_id)):
             if i >= len(test_dataset):
                 break
-            
+
             # for multi-person processing, set it to False
             if True:
                 test_data = test_dataset[i]
 
-                save_path = '%s/mResult/result_%s_%d.obj' % (opt.results_path,test_data['name'], opt.resolution)
+                save_path = '%s/%s_pifu.obj' % (opt.results_path, test_data['name'])
 
                 print(save_path)
-                gen_mesh_imgColor(opt.resolution, netMR, cuda, test_data, save_path, components=opt.use_compose)
+                gen_mesh(opt.resolution, netMR, cuda,
+                                  test_data, save_path, components=opt.use_compose)
             else:
                 for j in range(test_dataset.get_n_person(i)):
                     test_dataset.person_id = j
                     test_data = test_dataset[i]
-                    save_path = '%s/mResult/result_%s_%d.obj' % (opt.results_path, test_data['name'], j)
-                    gen_mesh(opt.resolution, netMR, cuda, test_data, save_path, components=opt.use_compose)
+                    save_path = '%s/result_%s_%d.obj' % (
+                        opt.results_path, test_data['name'], j)
+                    gen_mesh(opt.resolution, netMR, cuda, test_data,
+                             save_path, components=opt.use_compose)
+
 
 def reconWrapper(args=None, use_rect=False):
     opt = parser.parse(args)
     recon(opt, use_rect)
 
+
 if __name__ == '__main__':
     reconWrapper()
-  
--- apps/simplePifu.py
+++ apps/simplePifu.py
@@ -5,12 +5,15 @@ import glob
 from .recon import reconWrapper
 import argparse
 import trimesh
+import math
+import cv2
+import numpy as np
 ###############################################################################################
 ##                   Setting
 ###############################################################################################
 parser = argparse.ArgumentParser()
 parser.add_argument('-i', '--input_path', type=str, default='./simple_pifu')
-parser.add_argument('-o', '--out_path', type=str, default='./')
+parser.add_argument('-o', '--out_path', type=str, default='./simple_pifu')
 parser.add_argument('-c', '--ckpt_path', type=str, default='./checkpoints/pifuhd.pt')
 parser.add_argument('-r', '--resolution', type=int, default=512)
 parser.add_argument('--use_rect', action='store_true', help='use rectangle for cropping')
@@ -20,6 +23,7 @@ args = parser.parse_args()
 ###############################################################################################
 
 ## auto generate rect txt
+
 def autoTxt(path):
 	im = Image.open(path)
 	w, h = im.size
@@ -47,11 +51,89 @@ def meshcleaning(file_dir):
                 out_mesh = c
         out_mesh.export(obj_path)
 
-#get all image in folder & gen txt
-for imgs in glob.glob(args.input_path+"/*.jpg"):
-	autoTxt(imgs)
-for imgs in glob.glob(args.input_path+"/*.png"):
-	autoTxt(imgs)
+def normalDir(normal):
+		return math.copysign(1, normal[2]-0.5)
+
+## generate obj UV
+def genUV(texPath):
+	UV_obj = texPath.rpartition('.')[0]+'_UV.obj'
+	ori_obj = texPath.rpartition('.')[0]+'_pifu.obj'
+	
+	vertice = []
+	uv = []
+	normal = []
+	face = []
+	# Strips the newline character
+	Lines = open(ori_obj, 'r').readlines()
+	for line in Lines:
+		contents = line.split()
+		if contents[0] == 'v':
+			vertice.append([float(contents[1]), float(
+				contents[2]), float(contents[3])])
+			uv.append([float(contents[1])*0.5+0.5, float(contents[2])*0.5+0.5])
+			normal.append([float(contents[4]), float(
+				contents[5]), float(contents[6])])
+		elif contents[0] == 'f':
+			face.append([float(contents[1]), float(
+				contents[2]), float(contents[3])])
+	# Write Obj
+	outF = open(UV_obj, "w")
+	outF.write('mtllib sample.mtl\n')
+	outF.write('usemtl Material\n')
+	outF.write('s off\n')
+
+	for vertex in vertice:
+		outF.write("v {0} {1} {2}\n".format(vertex[0], vertex[1], vertex[2]))
+
+	for index,coord in enumerate(uv):
+		if(normalDir(normal[index])==1):
+			outF.write("vt {0} {1}\n".format(coord[0]/2.0, coord[1]))
+		else:
+			outF.write("vt {0} {1}\n".format(coord[0]/2.0+0.5, coord[1]))
+
+	for n in normal:
+		outF.write("vn {0} {1} {2}\n".format(n[0], n[1], n[2]))
+
+	for f in face:
+		f[0]=int(f[0])
+		f[1]=int(f[1])
+		f[2]=int(f[2])    
+		if(normalDir(normal[f[0]-1])==normalDir(normal[f[1]-1]) 
+		and normalDir(normal[f[1]-1])==normalDir(normal[f[2]-1])
+		and normalDir(normal[f[0]-1])==normalDir(normal[f[2]-1])
+		):
+			outF.write("f {0}/{0} {1}/{1} {2}/{2}\n".format(int(f[0]), int(f[1]), int(f[2])))
+		else:
+			outF.write("f {0} {1} {2}\n".format(int(f[0]), int(f[1]), int(f[2])))
+	outF.close()
+	# Write texture
+	sampleTexture = texPath.rpartition('.')[0]+'_tex.png'
+	texture = cv2.imread(texPath)
+	texture_gray = cv2.cvtColor(texture,cv2.COLOR_BGR2GRAY)
+	texture_gray = cv2.cvtColor(texture_gray,cv2.COLOR_GRAY2BGR)
+	vis = np.concatenate((texture, texture_gray), axis=1)
+	cv2.imwrite(sampleTexture, vis)
+	return
+	
+# make sure not to get pifu output to TODO
+def checkPath(path):
+	fileName = os.path.basename(path)
+	if fileName[-9:-4] == '_pifu':
+		return
+	if fileName[-8:-4] == '_tex':
+		return
+	todoPath.append(path)
+	
+#get all TODO img
+todoPath = []
+for img in glob.glob(args.input_path+"/*.jpg"):
+	checkPath(img)
+for img in glob.glob(args.input_path+"/*.png"):
+	checkPath(img)
+	
+# generate txt
+for todo in todoPath:
+	autoTxt(todo)
 
 #call pifu
 resolution = str(args.resolution)
@@ -65,4 +147,8 @@ reconWrapper(cmd, True)
 
 #clean mesh
 meshcleaning(args.out_path)
+
+#generate UV
+for todo in todoPath:
+	genUV(todo)
 
